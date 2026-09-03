c8f1c7b12d5e109abdd8f0d8ee593e3d36aff8a4
Sepehr <sfix2026@gmail.com>
2026-09-02 22:09:35 -0700

    fix mobile language switch

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import "./Navbar.css"
-import { useState } from "react"
+import { useState, useContext } from "react"
 import { Link } from "react-scroll"
-import { useContext } from "react"
 import { LanguageContext } from "../LanguageContext"
 
 function Navbar() {
@@ -9,14 +8,10 @@ function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false)
 
   const {
-
     toggleLanguage,
-    
     lang,
-    
     text
-    
-    } = useContext(LanguageContext)
+  } = useContext(LanguageContext)
 
   const closeMenu = () => {
     setMenuOpen(false)
@@ -26,92 +21,70 @@ function Navbar() {
 
     <nav className={`navbar ${menuOpen ? "menu-open" : ""}`}>
 
-<div className="nav-logo">
-  SFI<span className="logo-x">X</span>
-</div>
+      <div className="nav-logo">
+        SFI<span className="logo-x">X</span>
+      </div>
 
 
-<div className="nav-links"> 
+      <div className="nav-links">
 
+        <Link
+          to="about"
+          smooth={true}
+          duration={800}
+          onClick={closeMenu}
+        >
+          {text.navbar.about}
+        </Link>
 
-<Link 
-  to="about" 
-  smooth={true} 
-  duration={800} 
-  onClick={closeMenu} 
->
+        <Link
+          to="services"
+          smooth={true}
+          duration={800}
+          onClick={closeMenu}
+        >
+          {text.navbar.services}
+        </Link>
 
-{text.navbar.about}
+        <Link
+          to="portfolio"
+          smooth={true}
+          duration={800}
+          onClick={closeMenu}
+        >
+          {text.navbar.projects}
+        </Link>
 
-</Link> 
+        <Link
+          to="process"
+          smooth={true}
+          duration={800}
+          onClick={closeMenu}
+        >
+          {text.navbar.process}
+        </Link>
 
+        <Link
+          to="contact"
+          smooth={true}
+          duration={800}
+          onClick={closeMenu}
+        >
+          {text.navbar.contact}
+        </Link>
 
+      </div>
 
-<Link 
-  to="services" 
-  smooth={true} 
-  duration={800} 
-  onClick={closeMenu} 
->
-
-{text.navbar.services}
-
-</Link> 
-
-
-
-<Link 
-  to="portfolio" 
-  smooth={true} 
-  duration={800} 
-  onClick={closeMenu} 
->
-
-{text.navbar.projects}
-
-</Link> 
-
-
-
-<Link 
-  to="process" 
-  smooth={true} 
-  duration={800} 
-  onClick={closeMenu} 
->
-
-{text.navbar.process}
-
-</Link> 
-
-
-
-<Link 
-  to="contact" 
-  smooth={true} 
-  duration={800} 
-  onClick={closeMenu} 
->
-
-{text.navbar.contact}
-
-</Link> 
-
-
-</div>
 
       <div className="nav-right">
 
-      <span
-onClick={toggleLanguage}
-style={{
-cursor:"pointer"
-}}
->
+        <span
+          className="language-button"
+          onClick={toggleLanguage}
+        >
+          {lang === "en" ? "FA" : "EN"}
+        </span>
 
-{lang.toUpperCase()}
-
-</span>
 
         <button
           className="menu"
